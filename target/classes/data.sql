2b7612d1de1deda9ec59a7742c6cbe716377b546
Cristhyan Perez <93043890+cris-pevi@users.noreply.github.com>
2026-07-07 09:45:29 -0500

    add target

---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO candidato (id, nombre, partido) VALUES (1, 'Keiko Fujimori', 'Fuerza Popular');
+INSERT INTO candidato (id, nombre, partido) VALUES (2, 'Roberto Sánchez', 'Juntos por el Perú');
+
+INSERT INTO resultado (id, ambito, votos, porcentaje, candidato_id) VALUES (1, 'NACIONAL', 9223396, 50.135, 1);
+INSERT INTO resultado (id, ambito, votos, porcentaje, candidato_id) VALUES (2, 'NACIONAL', 9173755, 49.865, 2);
+
+INSERT INTO resultado (id, ambito, votos, porcentaje, candidato_id) VALUES (3, 'EXTRANJERO', 95000, 60.0, 1);
+INSERT INTO resultado (id, ambito, votos, porcentaje, candidato_id) VALUES (4, 'EXTRANJERO', 63000, 40.0, 2);
